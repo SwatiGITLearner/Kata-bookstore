0ffb1ed76891c5ba1f3a82ff7246a0e252d9cff8
swati <swati.nihal@gmail.com>
2024-10-20 19:20:27 +0200

    Pom swagger update

--- src/main/java/com/scs/kata/spring_boot_rest/SpringBootRestApplication.java
+++ src/main/java/com/scs/kata/spring_boot_rest/SpringBootRestApplication.java
@@ -2,11 +2,13 @@ package com.scs.kata.spring_boot_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootRestApplication {
 
 	/*@Bean
